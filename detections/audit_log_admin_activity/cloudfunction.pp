83fa0e165ec8c843f4a5674d821acb19808196a9
Priyanka Chatterjee <priyanka.chatterjee@turbot.com>
2024-12-12 01:02:23 +0530

    Remove unwanted detections

--- detections/audit_log_admin_activity/cloudfunction.pp
+++ detections/audit_log_admin_activity/cloudfunction.pp
@@ -5,8 +5,6 @@ locals {
 
   audit_log_admin_activity_detect_cloudfunctions_publicly_accessible_sql_columns      = replace(local.audit_log_admin_activity_detection_sql_columns, "__RESOURCE_SQL__", "resource_name")
   audit_log_admin_activity_detect_cloudfunctions_operation_delete_sql_columns         = replace(local.audit_log_admin_activity_detection_sql_columns, "__RESOURCE_SQL__", "resource_name")
-  audit_log_admin_activity_detect_cloudfunctions_invoked_by_pubsub_sql_columns        = replace(local.audit_log_admin_activity_detection_sql_columns, "__RESOURCE_SQL__", "resource_name")
-  audit_log_admin_activity_detect_cloudfunctions_invoked_by_job_scheduler_sql_columns = replace(local.audit_log_admin_activity_detection_sql_columns, "__RESOURCE_SQL__", "resource_name")
 }
 
 benchmark "audit_log_admin_activity_cloudfunction_detections" {
@@ -16,8 +14,6 @@ benchmark "audit_log_admin_activity_cloudfunction_detections" {
   children = [
     detection.audit_log_admin_activity_detect_cloudfunctions_publicly_accessible,
     detection.audit_log_admin_activity_detect_cloudfunctions_operation_delete,
-    detection.audit_log_admin_activity_detect_cloudfunctions_invoked_by_pubsub,
-    detection.audit_log_admin_activity_detect_cloudfunctions_invoked_by_job_scheduler,
   ]
 
   tags = merge(local.audit_log_admin_activity_cloudfunction_detection_common_tags, {
@@ -41,28 +37,6 @@ detection "audit_log_admin_activity_detect_cloudfunctions_operation_delete" {
   description = "Detect when Cloud Functions are deleted, enabling prompt action to prevent accidental loss of critical serverless resources or potential security issues caused by unauthorized deletions."
   severity    = "medium"
   query       = query.audit_log_admin_activity_detect_cloudfunctions_operation_delete
-
-  tags = merge(local.audit_log_admin_activity_detection_common_tags, {
-    mitre_attack_ids = "TA0002:T1648"
-  })
-}
-
-detection "audit_log_admin_activity_detect_cloudfunctions_invoked_by_pubsub" {
-  title       = "Detect Cloud Functions Invoked by Pub/Sub Topics"
-  description = "Detect when Cloud Functions are invoked by Pub/Sub topics, ensuring visibility into event-driven executions and monitoring for potential misuse or unexpected behaviors."
-  severity    = "medium"
-  query       = query.audit_log_admin_activity_detect_cloudfunctions_invoked_by_pubsub
-
-  tags = merge(local.audit_log_admin_activity_detection_common_tags, {
-    mitre_attack_ids = "TA0002:T1648"
-  })
-}
-
-detection "audit_log_admin_activity_detect_cloudfunctions_invoked_by_job_scheduler" {
-  title       = "Detect Cloud Functions Invoked by Job Schedulers"
-  description = "Detect when Cloud Functions are invoked by Job Schedulers, providing visibility into scheduled executions and ensuring they align with intended automation workflows."
-  severity    = "medium"
-  query       = query.audit_log_admin_activity_detect_cloudfunctions_invoked_by_job_scheduler
 
   tags = merge(local.audit_log_admin_activity_detection_common_tags, {
     mitre_attack_ids = "TA0002:T1648"
@@ -98,38 +72,6 @@ query "audit_log_admin_activity_detect_cloudfunctions_operation_delete" {
     where
       service_name = 'cloudfunctions.googleapis.com'
       and method_name ilike 'google.cloud.functions.v%.cloudfunctionsservice.deletefunction'
-      ${local.audit_log_admin_activity_detection_where_conditions}
-    order by
-      timestamp desc;
-  EOQ
-}
-
-query "audit_log_admin_activity_detect_cloudfunctions_invoked_by_pubsub" {
-  sql = <<-EOQ
-    select
-      ${local.audit_log_admin_activity_detect_cloudfunctions_invoked_by_pubsub_sql_columns}
-    from
-      gcp_audit_log_admin_activity
-    where
-      service_name = 'cloudfunctions.googleapis.com'
-      and method_name ilike 'google.cloud.functions.v%.cloudfunctionsservice.callfunction'
-      and cast(request_metadata->'caller_supplied_user_agent' as varchar) = 'Google-PubSub'
-      ${local.audit_log_admin_activity_detection_where_conditions}
-    order by
-      timestamp desc;
-  EOQ
-}
-
-query "audit_log_admin_activity_detect_cloudfunctions_invoked_by_job_scheduler" {
-  sql = <<-EOQ
-    select
-      ${local.audit_log_admin_activity_detect_cloudfunctions_invoked_by_job_scheduler_sql_columns}
-    from
-      gcp_audit_log_admin_activity
-    where
-      service_name = 'cloudfunctions.googleapis.com'
-      and method_name ilike 'google.cloud.functions.v%.cloudfunctionsservice.callfunction'
-      and cast(request_metadata -> 'caller_supplied_user_agent' as varchar) = 'Google-Cloud-Scheduler'
       ${local.audit_log_admin_activity_detection_where_conditions}
     order by
       timestamp desc;
--- mitre_v151/ta0002/ta0002_t1648.pp
+++ mitre_v151/ta0002/ta0002_t1648.pp
@@ -10,9 +10,7 @@ benchmark "mitre_v151_ta0002_t1648" {
   documentation = file("./mitre_v151/docs/ta0002_t1648.md")
   children = [
     detection.audit_log_admin_activity_detect_appengine_admin_api_execution_enabled,
-    detection.audit_log_admin_activity_detect_cloudfunctions_invoked_by_pubsub,
     detection.audit_log_admin_activity_detect_cloudfunctions_operation_delete,
-    detection.audit_log_admin_activity_detect_cloudfunctions_invoked_by_job_scheduler,
     detection.audit_log_admin_activity_detect_cloudfunctions_publicly_accessible,
   ]
 
