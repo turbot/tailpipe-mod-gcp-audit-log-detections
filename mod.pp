97c72577484aa4c4fc9e343d54cb59be19a72578
Cody Bruno <cody@turbot.com>
2025-01-30 08:45:31 -0500

    Add initial README and index doc, update mod.pp with latest format

--- mod.pp
+++ mod.pp
@@ -1,16 +1,16 @@
 mod "gcp_audit_log_detections" {
   # hub metadata
   title         = "GCP Audit Log Detections"
-  description   = "Search your GCP logs for high risk actions using Tailpipe."
+  description   = "Run detections and view dashboards for your GCP audit logs to monitor and analyze activity across your GCP projects using Powerpipe and Tailpipe."
   color         = "#ea4335"
-  #documentation = file("./docs/index.md")
-  #icon          = "/images/mods/turbot/gcp.svg"
+  documentation = file("./docs/index.md")
+  icon          = "/images/mods/turbot/gcp-audit-log-detections.svg"
   categories    = ["dashboard", "detections", "gcp", "public cloud"]
   database      = var.database
 
   opengraph {
-    title       = "Tailpipe Mod for GCP Audit Log Detections"
-    description = "Search your GCP logs for high risk actions using Tailpipe."
-    #image       = "/images/mods/turbot/gcp-social-graphic.png"
+    title       = "Powerpipe Mod for AWS CloudTrail Log Detections"
+    description = "Run detections and view dashboards for your GCP audit logs to monitor and analyze activity across your GCP projects using Powerpipe and Tailpipe."
+    image       = "/images/mods/turbot/gcp-audit-log-detections-social-graphic.png"
   }
 }
